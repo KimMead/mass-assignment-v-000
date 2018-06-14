c78c3c70a6fce1d39bac9e358a5010ad78e4a60f
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-06-14 01:40:25 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,6 @@ class Person
 
 
   def initialize(attributes)
-    attributes.find {|key, value| self.send(("#{key}="), value)}
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
